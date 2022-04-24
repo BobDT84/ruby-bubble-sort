86a27102987627fbfc18916dba2e282b730ac54b
Bob <bobdt84@gmail.com>
2022-04-23 21:50:13 -0500

    Rubocop autocorrect linter used

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,20 +1,20 @@
 def bubble_sort(array)
-    swaps = 1
-    loops = 0
-    until swaps == 0
-        loops += 1
-        swaps = 0
-        array.each_with_index do |value, index|
-            next_value = array[index + 1]
-            if (index + 1) < array.length && value > next_value
-                array[index] = next_value
-                array[index + 1] = value
-                swaps += 1
-            end
-        end
+  swaps = 1
+  loops = 0
+  until swaps == 0
+    loops += 1
+    swaps = 0
+    array.each_with_index do |value, index|
+      next_value = array[index + 1]
+      next unless (index + 1) < array.length && value > next_value
+
+      array[index] = next_value
+      array[index + 1] = value
+      swaps += 1
     end
-    puts loops
-    array
+  end
+  puts loops
+  array
 end
 
-p bubble_sort([4,3,78,2,0,2]) #=> [0,2,2,3,4,78]
+p bubble_sort([4, 3, 78, 2, 0, 2]) #=> [0,2,2,3,4,78]
